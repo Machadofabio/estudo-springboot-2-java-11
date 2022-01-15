5b4e7c006a9821bc7a071b8ffe7198bc6aefb514
Fabio Machado <fabio.a.machado@hotmail.com>
2022-01-14 21:24:26 -0300

    Criando entidade, controller, service e repository do orders

--- src/main/java/com/example/course/config/TestConfig.java
+++ src/main/java/com/example/course/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.example.course.config;
 
+import com.example.course.entities.Order;
 import com.example.course.entities.User;
+import com.example.course.repositories.OrderRepository;
 import com.example.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -15,6 +18,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -25,6 +31,18 @@ public class TestConfig implements CommandLineRunner {
         User u5 = new User(null, "Carlos Campos", "carlos.campos@hotmail.com", "1982909088", "senhacarlos");
         User u6 = new User(null, "Nicolas Chinaglia", "nicolas.chinaglia@hotmail.com", "19971521141", "senhanicolas");
 
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-08-22T15:21:22Z"), u3);
+        Order o4 = new Order(null, Instant.parse("2019-09-20T19:53:07Z"), u4);
+        Order o5 = new Order(null, Instant.parse("2019-10-21T03:42:10Z"), u5);
+        Order o6 = new Order(null, Instant.parse("2019-11-22T15:21:22Z"), u6);
+        Order o7 = new Order(null, Instant.parse("2019-12-20T19:53:07Z"), u1);
+        Order o8 = new Order(null, Instant.parse("2020-01-21T03:42:10Z"), u1);
+        Order o9 = new Order(null, Instant.parse("2020-02-22T15:21:22Z"), u2);
+
         userRepository.saveAll(Arrays.asList(u1, u2, u3, u4, u5, u6));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5, o6, o7, o8, o9));
     }
+
 }
